183be84789ac8b5b756d6796f3d34af2293bf523
huyiteng <961853266@qq.com>
2021-01-04 15:45:56 +0800

    fix some bugs in music player

--- app/src/main/java/com/example/countdowndays/NotificationSender.java
+++ app/src/main/java/com/example/countdowndays/NotificationSender.java
@@ -16,40 +16,41 @@ import androidx.annotation.RequiresApi;
 import com.example.countdowndays.model.Event;
 
 public class NotificationSender {
-    private static NotificationChannel mChannel = null;
     private static NotificationManager notificationManager = null;
-    private final static String ChannelID = "channel1";
-    private final static String ChannelName = "noti1";
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public NotificationSender(Context context){
-        mChannel = new NotificationChannel(ChannelID, ChannelName, NotificationManager.IMPORTANCE_HIGH);
         notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
     public static void  SendEventNotification(Event e, Context context){
 
+        NotificationChannel eventChannel = null;
+        String Channel_id = "Channel "+e.getId();
+        String Channel_name = "notification"+e.getId();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            eventChannel = new NotificationChannel(Channel_id,Channel_name,NotificationManager.IMPORTANCE_HIGH);
+        }
+
+
         Intent intent = new Intent(context,SplashScreenActivity.class);
         PendingIntent pit = PendingIntent.getActivity(context, 0, intent, 0);
-        //NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Notification notification = null;
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            //NotificationChannel mChannel = new NotificationChannel("channel1", "noti1", NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(mChannel);
-            setsound(context,e,mChannel);
-            notification = new Notification.Builder(context,ChannelID)
-                    .setChannelId(ChannelID)//通道ID
+            setsound(context,e,eventChannel);
+            notificationManager.createNotificationChannel(eventChannel);
+            notification = new Notification.Builder(context,Channel_id)
+                    .setChannelId(Channel_id)//通道ID
                     .setContentTitle("您设定的纪念日快到啦")//消息标题
                     .setTicker(e.getTitle())//
                     .setContentIntent(pit)//点击消息跳转
                     .setAutoCancel(true)//点击消息自动取消
-                    //.setWhen(System.currentTimeMillis())
-                    .setWhen(e.getNotidate())
+                    .setWhen(System.currentTimeMillis())
+                    //.setWhen(e.getNotidate())
                     .setContentText(e.getNote())//消息内容
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .build();//消息大图标
-
-            Log.d("1","channel");
+                    .setSmallIcon(R.mipmap.app_icon)
+                    .build();
 
         }
         notificationManager.notify(e.getId(), notification);
@@ -60,17 +61,19 @@ public class NotificationSender {
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private static void setsound(Context context,Event e,NotificationChannel channel){
+    private static void setsound(Context context,Event e,NotificationChannel notificationChannel){
         String[] music_arr = context.getResources().getStringArray(R.array.music);
         String musicname = music_arr[e.getBgm()];
-        Uri soundUri = Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.lemon);
+        Uri soundUri = Uri.parse("android.resource://" + context.getPackageName() + "/raw/"+musicname);
         Log.d("1",soundUri.toString());
         AudioAttributes audioAttributes = new AudioAttributes.Builder()
                 .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
                 .setUsage(AudioAttributes.USAGE_NOTIFICATION)
                 .build();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            channel.setSound(soundUri, audioAttributes);
+            Log.d("1","nnnnnnnn");
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            notificationChannel.setSound(soundUri,audioAttributes);
         }
     }
 
